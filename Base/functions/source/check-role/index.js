d73407deea78a9869dd3d8c7908f0220df694f53
Beardsley <mibeard@amazon.com>
2018-09-24 10:45:33 -0700

    New CustomResource function to check for the existence of a specified IAM role. We can use this function to set a conditional on whether deployment is happening in the Event Engine or not.

---
+++ Base/functions/source/check-role/index.js
@@ -0,0 +1,32 @@
+exports.handler = function(event, context) {
+	console.log("check-role invoked:\n" + JSON.stringify(event));
+	var response = require('cfn-response');
+	var responseData = {};
+	
+	if ('Delete' == event.RequestType) {
+		console.log("CloudFormation DELETE stack event. Skipping check role.");
+		return response.send(event, context, response.SUCCESS, responseData);
+	}
+	if ('Create' == event.RequestType || 'Update' == event.RequestType) {
+		var aws = require('aws-sdk');
+		var iam = new aws.IAM();
+		var params = {RoleName: event.ResourceProperties.RoleName};
+		iam.getRole(params, function(err, data) {
+			if (err) {
+				// IAM throws an error instead of returning NULL when it can't find the role
+				if ('NoSuchEntity' == err.code) {
+					responseData = {Role: ''};
+					return response.send(event, context, response.SUCCESS, responseData);
+				} else {
+					responseData = {Error: 'iam.getRole failed for role ' + params.RoleName};
+					console.log(responseData.Error, err);
+					return response.send(event, context, response.FAILED, responseData);
+				}
+			} else {
+				console.log("Success. Role " + params.RoleName + " exists");
+				responseData = {Role: data.Role.Arn};
+				return response.send(event, context, response.SUCCESS, responseData);
+			}
+		});
+	}
+};
