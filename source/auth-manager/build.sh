b79b8552ce14380988e96bc43bcbfed2886d81f4
Beardsley <mibeard@amazon.com>
2020-12-11 11:54:05 -0800

    Switching from Dockerhub base images to ECR Public Gallery to avoid throttling errors

--- source/auth-manager/build.sh
+++ source/auth-manager/build.sh
@@ -1,5 +1,5 @@
 mkdir build
-tar -cf - --exclude={node_modules,shared-modules/dynamodb-helper/node_modules,shared-modules/token-manager/node_modules,shared-modules/config-helper/node_modules} Dockerfile package.json package-lock.json server.js -C .. shared-modules | (cd build && tar xvf -)
+tar -cf - --exclude={node_modules,shared-modules/dynamodb-helper/node_modules,shared-modules/token-manager/node_modules,shared-modules/config-helper/node_modules} Dockerfile package.json server.js -C .. shared-modules | (cd build && tar xvf -)
 
 cd build
 docker image build -t auth-manager -f Dockerfile .
